38c877f7076cc2c2eaeb6d04b57092cf5f308d59
Adi <dihash@gmail.com>
2025-08-20 19:23:51 +0300

    Async user actions

--- src/store/actions/user.actions.js
+++ src/store/actions/user.actions.js
@@ -3,38 +3,37 @@ import { userService } from "../../services/user.service.js"
 import { SET_USER, SET_SCORE } from "../reducers/user.reducer.js"
 import { store } from "../store.js"
 
-export function login(credentials) {
+export async function login(credentials) {
     // console.log('credentials:', credentials)
-    return userService.login(credentials)
-        .then((user) => {
-            console.log('user login:', user)
-            store.dispatch({ type: SET_USER, user })
-        })
-        .catch((err) => {
-            console.log('user actions -> Cannot login', err)
-            throw err
-        })
+    try {
+        const user = await userService.login(credentials)
+        store.dispatch({ type: SET_USER, user })
+    }
+    catch (err) {
+        console.log('user actions -> Cannot login', err)
+        throw err
+    }
 }
 
-export function signup(credentials) {
-    return userService.signup(credentials)
-        .then((user) => {
-            store.dispatch({ type: SET_USER, user })
-        })
-        .catch((err) => {
-            console.log('user actions -> Cannot signup', err)
-            throw err
-        })
+export async function signup(credentials) {
+    try {
+        const user = await userService.signup(credentials)
+        store.dispatch({ type: SET_USER, user })
+    }
+    catch (err) {
+        console.log('user actions -> Cannot signup', err)
+        throw err
+    }
 }
 
-export function logout(credentials) {
-    return userService.logout(credentials)
-        .then(() => {
-            store.dispatch({ type: SET_USER, user: null })
-        })
-        .catch((err) => {
-            console.log('user actions -> Cannot logout', err)
-        })
+export async function logout(credentials) {
+    try {
+        await userService.logout(credentials)
+        store.dispatch({ type: SET_USER, user: null })
+    }
+    catch (err) {
+        console.log('user actions -> Cannot logout', err)
+    }
 }
 
 export function checkout(diff) {
@@ -55,5 +54,5 @@ export async function loadUsers() {
         store.dispatch({ type: SET_USERS, users })
     } catch (err) {
         console.log('UserActions: err in loadUsers', err)
-    } 
+    }
 }
